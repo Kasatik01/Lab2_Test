a0ca9f5d7759027d4821a3380c3bd4f54db69ab4
Kasatik01 <127666866+Kasatik01@users.noreply.github.com>
2023-05-19 15:22:45 +0700

    Project

---
+++ QA_Lab3_Belin/STP1/STP1/Precious_element.cs
@@ -0,0 +1,28 @@
+﻿using STP2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace STP1
+{
+    internal class Precious_element : Element
+    {
+        private double precious_cost;
+        public void Init(int v, double t, double c)  // метод инициализации
+        {
+            base.Init(v, t);
+            precious_cost = c;
+        }
+        public double Cost()
+        {
+            return Math.Round(tsena * ves + precious_cost*ves/100, 2, MidpointRounding.AwayFromZero);
+        }
+        public void Display()
+        {
+            Console.WriteLine("Вес элемента:" + ves.ToString()); ;
+            Console.WriteLine("Цена грамма элемента:" + tsena.ToString());
+            Console.WriteLine("Стоимость драг.примесей в 100г: " + precious_cost.ToString());
+        }
+    }
+}
---
+++ QA_Lab3_Belin/STP1/STP1/Real_number.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace STP1
+{
+    class Real_number : Number
+    {
+        private int znak;
+        public Real_number() :base()
+        {
+            znak = 1;
+        }
+        public Real_number(int X, int Y, int Z) :base(X,Y)
+        {
+            if (Z == 0 || Z == 1 || Z == -1)
+            {
+                znak = Z;
+            }
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine(Convert.ToString(znak*x) +
+                 '/' + Convert.ToString(y));
+        }
+        public override double Real()
+        {
+            return Math.Round(znak * (double)x / (double)y,3);
+        }
+    }
+}
